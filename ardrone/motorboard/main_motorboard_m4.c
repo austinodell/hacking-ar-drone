9bdaacfd8f4d0a1c43f2226fc8496fa869bb2487
austinodell <austin@odell.cc>
2014-10-07 14:03:20 -0400

    new motor controls

---
+++ ardrone/motorboard/main_motorboard_m4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>   /* Standard input/output definitions */
+#include <unistd.h>  /* UNIX standard function definitions */
+#include <pthread.h>
+#include <ctype.h>    /* For tolower() function */
+#include <math.h>
+
+#include "../util/type.h"
+#include "../util/util.h"
+#include "mot.h"
+
+
+int main()
+{
+  printf("All motors at 50%\r\n");
+	
+  mot_Init();
+  
+  float throttle1 = 0;
+  float throttle2 = 0;
+  float throttle3 = 0;
+  float throttle4 = .30;
+
+  mot_Run(throttle1,throttle2,throttle3,throttle4);
+
+  //yield to other threads
+  pthread_yield();
+
+  sleep(5);
+ 
+  mot_Close();
+
+  printf("\nDone...\n");
+  return 0;
+}
