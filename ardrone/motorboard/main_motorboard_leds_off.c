206c0480756236a7c23d0e67efa03f46040607fa
austinodell <austin@odell.cc>
2014-11-06 15:44:18 -0500

    added led_off function

---
+++ ardrone/motorboard/main_motorboard_leds_off.c
@@ -0,0 +1,28 @@
+#include <stdio.h>   /* Standard input/output definitions */
+#include <unistd.h>  /* UNIX standard function definitions */
+#include <pthread.h>
+#include <ctype.h>    /* For tolower() function */
+#include <math.h>
+
+#include "../util/type.h"
+#include "../util/util.h"
+#include "mot.h"
+
+
+int main()
+{
+  printf("LEDs off");
+	
+  mot_Init();
+
+  mot_SetLeds(MOT_LEDOFF,MOT_LEDOFF,MOT_LEDOFF,MOT_LEDOFF);
+
+  pthread_yield();
+
+  sleep(5);
+ 
+  mot_Close();
+
+  printf("\nDone...\n");
+  return 0;
+}
